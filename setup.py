05a1c4b466967c2fa836bf904517856cd5e7edd3
TUY3 <13001273104@163.com>
2022-06-30 19:38:41 +0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-setup(name='gym_dogfight3d',
-	  version='1.0.1',
-	  install_requires=['gym'])
+setup(
+	name='gym_dogfight3d',
+	version='1.0.1',
+	packages=find_packages(),
+	install_requires=['gym',
+					  'transforms3d'
+	],
+	description='a 3d air combat reinforcement learning environment',
+)
